c1f20c1a3fe5ab83e47350b957ffc97350ef483b
Bruno Tostes <btfjulio@hotmail.com>
2020-12-16 20:40:03 -0300

    create blue background component

---
+++ components/shared/BlueBackground/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from '../../../styles/Background.module.css';
+
+const Logo: React.FC = () => {
+  return (
+    <div className={styles.main}>
+
+    </div>
+  )
+}
+
+export default Logo;
