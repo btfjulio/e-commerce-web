c658356a9674789e02eb52214443e6d570d3af09
Bruno Tostes <btfjulio@hotmail.com>
2020-12-16 20:40:23 -0300

    create background component

---
+++ components/LoginForm/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from '../../../styles/Background.module.css';
+
+const Logo: React.FC = () => {
+  return (
+    <div>
+      Login Form
+    </div>
+  )
+}
+
+export default Logo;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import MainComponent from '../components/shared/MainComponent'
+import { useRouter } from 'next/router';
+
+const router = useRouter();
 
 const Home: React.FC = () => {
   return (
-    <h1>Hello World</h1>
+    <MainComponent>
+      <button onClick={() => router.push('/Auth/Login') }></button>
+    </MainComponent>
   )
 }
 
