aee404351ffd57b030ea95ad1e14ab0697eb5384
Bruno Tostes <btfjulio@hotmail.com>
2020-12-18 09:09:20 -0300

    finish sign in logic

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { AppProps } from 'next/app';
 
-
 import Head from 'next/head';
 
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from '../store';
+
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+toast.configure()
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '../styles/globals.css';
